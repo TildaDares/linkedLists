cf0cab890a4e56cd379c1367d8ea487c9bb318ce
Tilda Udufo <mathildaudufo@gmail.com>
2020-07-19 16:00:16 +0100

    Refactored

--- linked_lists.rb
+++ linked_lists.rb
@@ -1,152 +1,158 @@
+# frozen_string_literal: true
+
 require_relative 'node'
 class LinkedList
-    def initialize(value)
-        @head = Node.new(value, nil)
-    end
+  def initialize(value)
+    @head = Node.new(value, nil)
+  end
 
-    def append(value)
-        if @head.nil?
-            @head = Node.new(value, nil)
-        else
-            current_node = @head
-            until current_node.next_node == nil
-                current_node = current_node.next_node
-            end
-            current_node.next_node = Node.new(value, nil)
-        end
+  def append(value)
+    if @head.nil?
+      @head = Node.new(value, nil)
+    else
+      current_node = @head
+      current_node = current_node.next_node until current_node.next_node.nil?
+      current_node.next_node = Node.new(value, nil)
     end
+  end
 
-    def prepend(value)
-        if @head.nil?
-            @head = Node.new(value, nil)
-        else
-            current_node = @head
-            @head = Node.new(value, current_node)
-        end
+  def prepend(value)
+    if @head.nil?
+      @head = Node.new(value, nil)
+    else
+      current_node = @head
+      @head = Node.new(value, current_node)
     end
+  end
 
-    def size
-        return 'nil' if @head.nil?
-        current_node = @head
-        count = 1
-        until current_node.next_node == nil
-            current_node = current_node.next_node
-            count += 1
-        end
-        count
-    end
+  def size
+    return 'nil' if @head.nil?
 
-    def to_s
-        return 'nil' if @head.nil?
-        elements = []
-        current_node = @head
-        until current_node == nil
-            elements << current_node.value
-            current_node = current_node.next_node
-        end
-        elements << 'nil'
-        elements.each do |value|
-            value == 'nil' ? (print "#{value} \n") : (print "#{value} -> ")
-        end
-        nil
+    current_node = @head
+    count = 1
+    until current_node.next_node.nil?
+      current_node = current_node.next_node
+      count += 1
     end
+    count
+  end
 
-    def head
-        return nil if @head.nil?
-        @head.value
-    end
+  def to_s
+    return 'nil' if @head.nil?
 
-    def tail
-        return 'nil' if @head.nil?
-        current_node = @head
-        until current_node.next_node == nil
-            current_node = current_node.next_node
-        end
-        current_node.value
+    elements = []
+    current_node = @head
+    until current_node.nil?
+      elements << current_node.value
+      current_node = current_node.next_node
     end
+    elements << 'nil'
+    elements.each do |value|
+      value == 'nil' ? (print "#{value} \n") : (print "#{value} -> ")
+    end
+    nil
+  end
 
-    def pop
-        return 'nil' if @head.nil?
-        current_node = @head
-        until current_node.next_node.next_node == nil
-            current_node = current_node.next_node
-        end
-        popped_value = current_node.next_node.value
-        current_node.next_node = nil
-        popped_value
-    end
+  def head
+    return nil if @head.nil?
 
-    def contains?(value)
-        current_node = @head
-        until current_node == nil
-            return true if current_node.value == value
-            current_node = current_node.next_node
-        end
-        false
-    end
+    @head.value
+  end
 
-    def find(value)
-        current_node = @head
-        count = 0
-        until current_node == nil
-            return count if current_node.value == value
-            current_node = current_node.next_node
-            count += 1
-        end
-        return 'nil'
-    end
+  def tail
+    return 'nil' if @head.nil?
 
-    def at(index)
-        current_node = @head
-        count = 0
-        until current_node== nil
-            return current_node.value if index == count
-            current_node = current_node.next_node
-            count += 1
-        end
-        return 'nil'
-    end
+    current_node = @head
+    current_node = current_node.next_node until current_node.next_node.nil?
+    current_node.value
+  end
 
-    def insert_at(value, index)
-        return 'nil' if @head.nil?
-        current_node = @head
-        count = 0
-        if index == 0
-            @head = Node.new(value, current_node)
-            return
-        end
-        prev_node = current_node
-        until current_node == nil
-            if index == count
-                new_node = Node.new(value, current_node)
-                prev_node.next_node = new_node
-                return
-            end
-            prev_node = current_node
-            current_node = current_node.next_node
-            count += 1
-        end
-    end
+  def pop
+    return 'nil' if @head.nil?
 
-    def remove_at(index)
-        return 'nil' if @head.nil?
-        current_node = @head
-        count = 0
-        if index == 0
-            @head = current_node.next_node
-            return current_node.value
-        end
-        prev_node = current_node
-        until current_node == nil
-            if index == count
-                prev_node.next_node = current_node.next_node
-                return current_node.value
-            end
-            prev_node = current_node
-            current_node = current_node.next_node
-            count += 1
-        end
+    current_node = @head
+    current_node = current_node.next_node until current_node.next_node.next_node.nil?
+    popped_value = current_node.next_node.value
+    current_node.next_node = nil
+    popped_value
+  end
+
+  def contains?(value)
+    current_node = @head
+    until current_node.nil?
+      return true if current_node.value == value
+
+      current_node = current_node.next_node
     end
+    false
+  end
+
+  def find(value)
+    current_node = @head
+    count = 0
+    until current_node.nil?
+      return count if current_node.value == value
+
+      current_node = current_node.next_node
+      count += 1
+    end
+    'nil'
+  end
+
+  def at(index)
+    current_node = @head
+    count = 0
+    until current_node.nil?
+      return current_node.value if index == count
+
+      current_node = current_node.next_node
+      count += 1
+    end
+    'nil'
+  end
+
+  def insert_at(value, index)
+    return 'nil' if @head.nil?
+
+    current_node = @head
+    count = 0
+    if index == 0
+      @head = Node.new(value, current_node)
+      return
+    end
+    prev_node = current_node
+    until current_node.nil?
+      if index == count
+        new_node = Node.new(value, current_node)
+        prev_node.next_node = new_node
+        return
+      end
+      prev_node = current_node
+      current_node = current_node.next_node
+      count += 1
+    end
+  end
+
+  def remove_at(index)
+    return 'nil' if @head.nil?
+
+    current_node = @head
+    count = 0
+    if index == 0
+      @head = current_node.next_node
+      return current_node.value
+    end
+    prev_node = current_node
+    until current_node.nil?
+      if index == count
+        prev_node.next_node = current_node.next_node
+        return current_node.value
+      end
+      prev_node = current_node
+      current_node = current_node.next_node
+      count += 1
+    end
+  end
 end
 lists = LinkedList.new(4)
 lists.append(5)
@@ -156,9 +162,9 @@ lists.append(2)
 lists.prepend(12)
 lists.prepend(45)
 puts lists.pop
-print "#{lists.to_s} "
-puts lists.contains? (12)
-puts lists.contains? (78)
+print "#{lists} "
+puts lists.contains? 12
+puts lists.contains? 78
 puts lists.find(78)
 puts lists.find(4)
 puts lists.find(90)
@@ -169,6 +175,6 @@ puts lists.size
 puts lists.head
 puts lists.tail
 lists.insert_at(567, 3)
-print "#{lists.to_s} "
+print "#{lists} "
 lists.remove_at(2)
-print "#{lists.to_s} "
+print "#{lists} "
--- node.rb
+++ node.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class Node
-    attr_accessor :value, :next_node
-    def initialize(value, next_node)
-      @value = value
-      @next_node = next_node
-    end
+  attr_accessor :value, :next_node
+  def initialize(value, next_node)
+    @value = value
+    @next_node = next_node
+  end
 end
